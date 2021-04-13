7d69de5799ba96724330f9c461e569f791af6043
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-13 17:56:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,7 @@ class Application
   def cart(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
     if req.path.match(/cart/)
       @@cart.each do |cart|
         resp.write "#{cart}/n"
